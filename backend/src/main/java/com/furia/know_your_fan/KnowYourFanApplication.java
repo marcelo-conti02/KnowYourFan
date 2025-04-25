5fe01fd0d990d87a7e90c3e5349b10617096e456
marcelo <marceloconti9017@gmail.com>
2025-04-25 13:39:03 -0300

    :sparkles: feat: Create Spring Boot app

---
+++ backend/src/main/java/com/furia/know_your_fan/KnowYourFanApplication.java
@@ -0,0 +1,13 @@
+package com.furia.know_your_fan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowYourFanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowYourFanApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/furia/know_your_fan/KnowYourFanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furia.know_your_fan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowYourFanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
