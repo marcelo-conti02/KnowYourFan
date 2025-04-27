d2d24a70db3c6c65c8b1b55373baf319f6192751
marcelo <marceloconti9017@gmail.com>
2025-04-27 12:48:24 -0300

    :sparkles: feat: Create UserRepository.java

---
+++ backend/src/main/java/com/furia/know_your_fan/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.furia.know_your_fan.repository;
+
+import com.furia.know_your_fan.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(Long email);
+    Optional<User> findByCpf(String cpf);
+}
